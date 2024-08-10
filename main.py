10eef53e68e20a04200191350ca5144a031eb390
David Green <hellodg.py@gmail.com>
2024-08-10 11:39:58 -0700

    Update game play algorithm to catch all wins and draws. Close off infinite loops. Next steps is to update user experience.

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ import random
 
 spaces_played = []
 
+# TO DO: When PC whens, it doesn't show the board
+# TO DO: If User whens, PC should not be able to place symbol on board again.
+# TO DO: Game ends when PC wins
+# TO DO: When its a draw at 9, it just keeps printing the board and doesn't stop.
+# TO DO: Ask user if they would like to play again.
+# TO DO: Change difficulty level by having PC block user when able.
+
 
 def game_grid(sp1, sp2, sp3, sp4, sp5, sp6, sp7, sp8, sp9):
     top_vert_line = f" {sp7} | {sp8} | {sp9} "
@@ -16,7 +23,7 @@ def game_grid(sp1, sp2, sp3, sp4, sp5, sp6, sp7, sp8, sp9):
 def check_for_win(sp1, sp2, sp3, sp4, sp5, sp6, sp7, sp8, sp9):
     winning_plays = [(sp1, sp2, sp3), (sp4, sp5, sp6), (sp7, sp8, sp9),
                      (sp1, sp4, sp7), (sp2, sp5, sp8), (sp3, sp6, sp9),
-                     (sp1, sp5, sp9), (sp7, sp5, sp8)]
+                     (sp1, sp5, sp9), (sp7, sp5, sp3)]
 
     if any(play == ("X", "X", "X") for play in winning_plays):
         print("\nX Wins the Game!")
@@ -25,7 +32,7 @@ def check_for_win(sp1, sp2, sp3, sp4, sp5, sp6, sp7, sp8, sp9):
         print("\nO Wins the Game!")
         return True
     else:
-        return
+        return False
 
 
 def game_play(user_symbol, pc_symbol):
@@ -34,37 +41,41 @@ def game_play(user_symbol, pc_symbol):
     playing_game = True
     while playing_game:
         users_turn = True
-        while users_turn and len(spaces_played) < 9:
+        while users_turn:
             if check_for_win(space[1], space[2], space[3], space[4], space[5], space[6], space[7], space[8], space[9]):
                 playing_game = False
                 users_turn = False
             elif len(spaces_played) == 9:
-                print("It was a draw!")
+                print("\nIt was a draw!")
+                users_turn = False
                 playing_game = False
 
             user_space_chosen = int(input("What space do you want to put your symbol? "))
-            if user_space_chosen not in spaces_played:
+            if user_space_chosen not in spaces_played and users_turn:
                 if user_space_chosen in space:
                     space[user_space_chosen] = user_symbol
                     spaces_played.append(user_space_chosen)
                     users_turn = False
 
         pc_turn = True
-        while pc_turn and len(spaces_played) < 9:
+        while pc_turn:
             if check_for_win(space[1], space[2], space[3], space[4], space[5], space[6], space[7], space[8], space[9]):
                 playing_game = False
                 pc_turn = False
             elif len(spaces_played) == 9:
-                print("It was a draw!")
+                print("\nIt was a draw!")
+                pc_turn = False
                 playing_game = False
 
             pc_space_chosen = random.randint(1,9)
-            if pc_space_chosen not in spaces_played:
+            if pc_space_chosen not in spaces_played and pc_turn:
                 if pc_space_chosen in space:
                     space[pc_space_chosen] = pc_symbol
                     spaces_played.append(pc_space_chosen)
                     pc_turn = False
 
+        # This game_grid shows after user wins because all code in PC block was False. But when PC wins
+        # this will not show because PC's win is checked in users code so we never make it here again.
         game_grid(space[1], space[2], space[3], space[4], space[5], space[6], space[7], space[8], space[9])
 
     return
